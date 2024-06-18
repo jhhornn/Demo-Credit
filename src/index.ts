1360e5538ef2beb608c647c7d098b191cf22035f
Awosise Oluwaseun <awosiseo@gmail.com>
2024-06-18 18:25:36 +0100

    Update index.ts to read env file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { config as dotenvConfig } from 'dotenv';
 import app from './app';
 import Config from './config';
+
+dotenvConfig();
 
 const PORT = Config.env_var.dev.serverConfig.PORT || 3000;
 
